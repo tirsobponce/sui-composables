fc5d8ca7f494d5ce02d0a62a05b6e9408617c527
Tirso J. Bello Ponce <tirsobponce@gmail.com>
2024-05-22 21:56:44 -0300

    Add API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,1 @@
-// src/index.js
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app: Express = express();
-const port = 3001;
-
-app.get("/", (_: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+console.log("Hola!");
